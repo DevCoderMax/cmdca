408a4f020184830e7c7c6cb97363e324d4fd0212
DevCoderMax <gleysondonascimentocarvalho@gmail.com>
2025-02-20 12:14:21 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: './',  // Isso fará com que o Vite use caminhos relativos
   server: {
-    port: 3090
+    port: 3090,
+    host: '0.0.0.0'
   },
   preview: {
-    port: 3090
+    port: 3090,
+    host: '0.0.0.0'
   }
 })
